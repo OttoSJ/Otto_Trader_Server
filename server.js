a5ff1abe1358bce3136bfcf6ef298b6a51518060
Otto Jones <ottosjonesjr@gmail.com>
2022-05-14 14:53:43 +0300

    heroku deployment

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 require("dotenv").config();
 const express = require("express");
-// const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 connectDB();
+
 
 // CONFIGURATION
 // const { errorHandler } = require("");
@@ -17,10 +17,10 @@ const app = express();
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(methodOverride("_method"));
+
 
 // ROUTES
-// app.use("/api/sellers", require("./models/seller_seed"));
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/inventory", require("./routes/carRoutes"));
 
